b9ca0847967b1e6423fbaaee6428cf0e0609ab0a
Kleber R Rovai <37271527+klrovai@users.noreply.github.com>
2023-12-19 12:13:19 -0300

    Add files via upload

---
+++ projeto.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+snome = gets.chomp
+
+print "Informe sua idade: "
+idade = gets.chomp
+
+puts "Seu nome é: #{nome} #{snome}, idade de #{idade} anos."
